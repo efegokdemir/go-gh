853db353ec174016ed14f53c626b94db2d42fd9c
Efe Gökdemir <efe@rexcode.co.uk>
2026-09-24 15:33:04 +0300

    fix(jq): preserve scalar number precision
    Signed-off-by: Efe Gokdemir <efegokdemir@users.noreply.github.com>

--- pkg/jq/jq_test.go
+++ pkg/jq/jq_test.go
@@ -46,6 +46,16 @@ func TestEvaluateFormatted(t *testing.T) {
 			wantW: "Mona\n8\n",
 		},
 		{
+			name: "preserves scalar precision",
+			args: args{
+				json:     strings.NewReader(`{"pi":3.14159,"small":0.001,"big":123456.789}`),
+				expr:     `.pi,.small,.big`,
+				indent:   "",
+				colorize: false,
+			},
+			wantW: "3.14159\n0.001\n123456.789\n",
+		},
+		{
 			name: "object as JSON",
 			args: args{
 				json:     strings.NewReader(`{"user":{"login":"monalisa"}}`),
